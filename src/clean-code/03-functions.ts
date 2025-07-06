74ce7871cda8aa9f559a221b636aa52eb7c70660
PatricioOsorio <patricio.osorio@elektra.com.mx>
2025-07-06 11:29:45 -0600

    ✨ feat: exercise 3 - functions

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,39 @@
+(() => {
+  // función para obtener información de una película por Id
+  function getMovieById(movieId: string) {
+    console.log({ movieId });
+  }
+
+  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+  function getMovieCastById(movieId: string) {
+    console.log({ movieId });
+  }
+
+  // funcion para obtener el bio del actor por el id
+  function getActorBioById(ActorId: string) {
+    console.log({ ActorId });
+  }
+
+  // Crear una película
+  interface IMovie {
+    title: string;
+    description: string;
+    rating: number;
+    cast: string[];
+  }
+
+  function createMovie({ title, description, rating, cast }: IMovie) {
+    console.log({ title, description, rating, cast });
+  }
+
+  // Crea un nuevo actor
+  function createActors(fullName: string, birthdate: Date): boolean {
+    // tarea asincrona para verificar nombre
+    // ..
+    // ..
+    if (fullName === 'fernando') return false;
+
+    console.log('Crear actor');
+    return true;
+  }
+})();
