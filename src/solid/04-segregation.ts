f64c543ed6b3c2aa210bbc5f48c9bfe2050b04e6
Patricio Miguel Osorio Osorio <patricio.osorio@agilityps.mx>
2026-02-18 12:13:55 -0600

    ✨ feat (Sección 5: Principios SOLID): Aplicar el principio de segregación de interfaz

--- src/solid/04-segregation.ts
+++ src/solid/04-segregation.ts
@@ -1,44 +1,37 @@
+// ! Interfaces
 export interface IBird {
-  fly(): void;
   eat(): void;
-  run(): void;
+}
+
+export interface IFlyingBird {
+  fly(): void;
+}
+
+export interface ISwimmingBird {
   swim(): void;
 }
 
-class Toucan implements IBird {
+export interface IRunningBird {
+  run(): void;
+}
+
+// ! Classes
+class Toucan implements IBird, IFlyingBird {
   public fly() {}
   public eat() {}
-  public run() {}
-  public swim() {
-    throw new Error("can't swim");
-  }
 }
 
-class HummingBird implements IBird {
+class HummingBird implements IBird, IFlyingBird {
   public fly() {}
   public eat() {}
-  public run() {}
-  public swim() {
-    throw new Error("can't swim");
-  }
 }
 
-class Ostrich implements IBird {
-  fly() {
-    throw new Error("can't fly");
-  }
+class Ostrich implements IBird, IRunningBird {
   public eat() {}
   public run() {}
-  public swim() {
-    throw new Error("can't swim");
-  }
 }
 
-class Penguin implements IBird {
-  fly() {
-    throw new Error("can't fly.");
-  }
+class Penguin implements IBird, ISwimmingBird {
   public eat() {}
-  public run() {}
   public swim() {}
 }
